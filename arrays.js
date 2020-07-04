e216f3ff2124d06d6687b11c91fd7e47af11b28e
Kenny Davis <kingdavis2423@gmail.com>
2020-07-04 01:37:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  chocolateBars.unshift(element)
+  array.unshift(element)
   return array
 }
 
